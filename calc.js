d083aaf11639435371f1749aaba1e06fb0aa2192
Patrick Calagos <edriannecalagos@gmail.com>
2019-12-05 19:19:41 +0800

    add files

--- calc.js
+++ calc.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
-    $('.calc_button').click(function() {
-      var current = ("$da_display").val();
-      $("$da_display").val(current + $(this).text());
+    $('.num_button').click(function() {
+      var current = $("#da_display").val();
+
+      if (current == '0'){
+          current = "";
+      }
+      $("#da_display").val(current + $(this).text());
     });
 });
