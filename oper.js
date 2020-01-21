4db4b2dfd08ca8076e08e1d127a7a809aa64271c
Patrick Calagos <edriannecalagos@gmail.com>
2020-01-21 19:35:01 +0800

    added files

--- oper.js
+++ oper.js
@@ -1,8 +1,31 @@
 $(document).ready(function(){
     var sel = document.createElement("select");
+
     var opt = document.createElement("option");
     $(opt).html("+");
     $(opt).val("plus");
     $(sel).append(opt);
+
+    var opt = document.createElement("option");
+    $(opt).html("-");
+    $(opt).val("minus");
+    $(sel).append(opt);
+
+    var opt = document.createElement("option");
+    $(opt).html("X");
+    $(opt).val("multiply");
+    $(sel).append(opt);
+
+    var opt = document.createElement("option");
+    $(opt).html("÷");
+    $(opt).val("divide");
+    $(sel).append(opt);
+
+    var opt = document.createElement("option");
+    $(opt).html("√");
+    $(opt).val("squareroot");
+    $(sel).append(opt);
+
     $("#ops").append(sel);
+
 })
